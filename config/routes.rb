10143b68b334ec2c6246a4e5676711f7930475a3
Arisa NAGAO <arisa@ArisanoMacBook.local>
2017-12-23 01:27:38 +0900

    letter_opner設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts, only: %i[index new create edit update destroy]
   root 'top#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at:"/letter_opener"
+  end
 end
